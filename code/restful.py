f6b9244ca2418cd3df7a204fd71ee8afce544389
Dean Hope Robertson <deanhoperobertson@gmail.com>
2022-03-02 13:10:32 +0000

    Created ItemList and http error codes

--- code/restful.py
+++ code/restful.py
@@ -5,14 +5,33 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class Student(Resource):#inherets from Resource class
+#in memory data store
+items =[]
+
+
+class Item(Resource):#inherets from Resource class
 
 	#GET method
 	def get(sel, name):
-		return {"student" : name}
+		for item in items:
+			if item["name"]==name:
+				return item
+		return {"item": None}, 404 #return null and 404 (not found) http status code
+
+
+	def post(self, name):
+		item = {"name": name, "price": 12}
+		items.append(item)
+		return item, 201 #return item and 201 (created) http status code
+
+
+class Itemlist(Resource):
+	def get(self):
+		return {"items": items}
+
 
 #add class resouce to api and define endpoint (similar to @app.route())
-api.add_resource(Student, "/student/<string:name>")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(Itemlist, "/items/")
 
-
-app.run(port=5001)
+app.run(port=5001, debug=True)
