6182dadcc39e5e95a17de4359cabf5f9cf8103b5
Dean Hope Robertson <deanhoperobertson@gmail.com>
2022-03-23 20:18:05 +0000

    used next filter in get method

--- code/restful.py
+++ code/restful.py
@@ -14,14 +14,22 @@ class Item(Resource):#inherets from Resource class
 
 	#GET method
 	def get(sel, name):
-		for item in items:
-			if item["name"]==name:
-				return item
-		return {"item": None}, 404 #return null and 404 (not found) http status code
+		
+		# filter func returns iterator, so need next() to return single item.
+		# add None so next() doesnt raise error if there are no items
+		item = next(filter(lambda x: x["name"]==name, items), None)
+		# for item in items:
+		# 	if item["name"]==name:
+		# 		return item
+		return {"item": None}, 200 if item else 404 #return null and 404 (not found) http status code
 
 
 	#POST Method
 	def post(self, name):
+
+		if next(filter(lambda x: x["name"]==name, items), None) is not None:
+			return {"message": "An item with name '{}'already exists".format(name)}
+
 		data = request.get_json() #force=True 
 
 		item = {"name": name, "price": data["price"]}
